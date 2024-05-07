735f12396a0330a5a17d72197d458a7692f6848e
Ryan Zielke <NeoAlchemy@gmail.com>
2024-05-06 19:56:41 -0500

    setting up azure functions
    setting up azure functions

--- api/function_app.py
+++ api/function_app.py
@@ -19,7 +19,7 @@ def message(req: func.HttpRequest) -> func.HttpResponse:
             
             client = OpenAI()
             response = client.chat.completions.create(
-                model="gpt-4-turbo",
+                model="gpt-3.5-turbo",
                 messages=[
                     {
                     "role": "system",
