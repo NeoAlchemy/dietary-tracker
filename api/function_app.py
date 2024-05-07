087adcb45b15bc0377dd356a4f537db389eb09b1
Ryan Zielke <NeoAlchemy@gmail.com>
2024-05-06 20:22:18 -0500

    initialize response
    fixed response value

--- api/function_app.py
+++ api/function_app.py
@@ -9,6 +9,8 @@ def message(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     input = req.params.get('input')
+    response = ''
+    
     if not input:
         try:
             req_body = req.get_json()
